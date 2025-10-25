370d9af3eeb5856a9e88806814486ad543562ade
Maurice Lorenz <me@mauricelorenz.com>
2025-10-25 15:22:31 +0200

    Add libft.h and Makefile for including libft

---
+++ libft/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/25 14:59:57 by mlorenz           #+#    #+#             */
+/*   Updated: 2025/10/25 15:00:08 by mlorenz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+
+#endif
