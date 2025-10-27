f612056d931f81cf29ea0462478832fbd2eb2bf3
Maurice Lorenz <mlorenz@student.42heilbronn.de>
2025-10-27 20:41:55 +0100

    Add basic logic to ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,28 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 14:45:55 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/10/25 15:20:12 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/10/27 18:52:32 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *str, ...)
 {
-	
+	va_list	args;
+
+	va_start(args, str);
+	while (*str)
+	{
+		if (*str == '%' && *(str + 1) != '%')
+		{
+			if ()
+			str = str + 2;
+		}
+		else
+		{
+			write(1, *str, 1);
+			str++;
+		}
+	}
 }
